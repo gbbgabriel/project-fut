1c39b921f1b8efbb072e885bf43321bdf77e17a9
Gbb <gabrielbarruecobotelho10@gmail.com>
2023-03-12 21:26:44 -0300

    error correction

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminModule } from './admin/admin.module';
 import { AdmModule } from './adm/adm.module';
 import { GroupsModule } from './groups/groups.module';
 import { TeamModule } from './team/team.module';
 import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [AdminModule, AdmModule, GroupsModule, TeamModule, PlayerModule],
+  imports: [AdmModule, GroupsModule, TeamModule, PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
