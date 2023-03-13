c38d5d9df66346b9c368fec5fe09da88d1dd65b4
Gbb <gabrielbarruecobotelho10@gmail.com>
2023-03-12 21:59:04 -0300

    create crud groups

--- src/groups/dto/create-group.dto.ts
+++ src/groups/dto/create-group.dto.ts
@@ -1,1 +1,11 @@
-export class CreateGroupDto {}
+import { IsString } from 'class-validator';
+import { v4 as uuidv4 } from 'uuid';
+import { Transform } from 'class-transformer';
+
+export class CreateGroupDto {
+  @Transform(() => uuidv4())
+  id: string;
+
+  @IsString()
+  name: string;
+}
--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 
 @Injectable()
 export class GroupsService {
-  create(createGroupDto: CreateGroupDto) {
-    return 'This action adds a new group';
+  constructor(private readonly database: PrismaService) {}
+
+  async create(createGroupDto: CreateGroupDto) {
+    return this.database.group.create({
+      data: {
+        name: createGroupDto.name,
+      },
+    });
+    //return 'This action adds a new group';
   }
 
-  findAll() {
-    return `This action returns all groups`;
+  async findAll() {
+    return this.database.group.findMany();
+    //return `This action returns all groups`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} group`;
+  async findOne(id: string) {
+    return this.database.group.findFirst({
+      where: {
+        id,
+      },
+    });
+    //return `This action returns a #${id} group`;
   }
 
-  update(id: number, updateGroupDto: UpdateGroupDto) {
-    return `This action updates a #${id} group`;
+  async update(id: string, updateGroupDto: UpdateGroupDto) {
+    return this.database.group.update({
+      where: {
+        id,
+      },
+      data: {
+        name: updateGroupDto.name,
+      },
+    });
+    //return `This action updates a #${id} group`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} group`;
+  async remove(id: string) {
+    return this.database.group.delete({ where: { id } });
+    //return `This action removes a #${id} group`;
+  }
+
+  async exists(id: string) {
+    if (!(await this.findOne(id))) {
+      throw new NotFoundException(`O usuário ${id} não existe`);
+    }
   }
 }
